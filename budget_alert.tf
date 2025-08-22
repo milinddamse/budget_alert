cc476ae16e489bceff6fae2b79c1c34565cd249b
milinddamse <milind.damse@gmail.com>
2025-08-22 17:17:49 +0530

    Update budget_alert.tf

--- budget_alert.tf
+++ budget_alert.tf
@@ -1,47 +1,92 @@
-# Variable for the resource group name
-variable "resource_group_name" {
-  description = "The name of the resource group containing the VMSS."
-  type        = string
-  default     = "rg-vmss-budget-demo"
-}
- 
-# Variable for the VMSS name
-variable "vmss_name" {
-  description = "The name of the VMSS to monitor."
-  type        = string
-  default     = "vmss-budget-demo"
-}
- 
-# Budget alert configuration
-resource "azurerm_consumption_budget_subscription" "vmss_budget" {
-  name            = "vmss-cost-budget"
-  amount          = 50  # Set your desired budget amount in USD
-  time_grain      = "Daily"
-  time_period_start = "2025-08-01T00:00:00Z"
- 
-  # Filter to specifically monitor costs for the VMSS
-  filter {
-    resource_group {
-      name = var.resource_group_name
-    }
- 
-    resource {
-      name = var.vmss_name
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
     }
   }
- 
-  # Action group to receive the alert
-  notification {
-    enabled = true
-    operator = "GreaterThan"
-    threshold = 90  # Alert when 90% of the budget is consumed
-    threshold_type = "Actual"
-    contact_emails = ["milind.damse@gmail.com"]
-  }
-}
- 
-output "budget_id" {
-  value = azurerm_consumption_budget_subscription.vmss_budget.id
-  description = "The ID of the created budget alert."
 }
 
+
+provider "azurerm" {
+  features {}
+  client_id = "4f703a45-8f95-4190-a2df-3107ec0059e1"
+  client_secret = "SxQ8Q~hhJhPetxca.XoAjL0Sj6TEZPlbHVDEna0F"
+  tenant_id = "5e865d62-8ae0-4163-9923-646bf3b4ffa1"
+  subscription_id = "a01bd4dc-3157-43b6-a8f9-a9a3c40a0e8c" # Optional if using Azure CLI logn
+}
+
+
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "milindRg3"
+  location = "eastus"
+}
+
+resource "azurerm_monitor_action_group" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+  short_name          = "example"
+}
+
+resource "azurerm_consumption_budget_subscription" "example" {
+  name            = "example"
+  subscription_id = data.azurerm_subscription.current.id
+
+  amount     = 1000
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2025-08-01T00:00:00Z"
+    end_date   = "2025-09-01T00:00:00Z"
+  }
+
+  filter {
+    dimension {
+      name = "ResourceGroupName"
+      values = [
+        azurerm_resource_group.example.name,
+      ]
+    }
+
+    tag {
+      name = "foo"
+      values = [
+        "bar",
+        "baz",
+      ]
+    }
+  }
+
+  notification {
+    enabled   = true
+    threshold = 90.0
+    operator  = "EqualTo"
+
+    contact_emails = [
+      "milind.damse@gmail.com"
+     
+    ]
+
+    contact_groups = [
+      azurerm_monitor_action_group.example.id,
+    ]
+
+    contact_roles = [
+      "Owner",
+    ]
+  }
+
+  notification {
+    enabled        = false
+    threshold      = 100.0
+    operator       = "GreaterThan"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "milind.damse@gmail.com"
+    ]
+  }
+}
