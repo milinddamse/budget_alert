16619fe99cd3dce34e880113addbead3d30d3b84
milinddamse <milind.damse@gmail.com>
2025-08-22 17:01:25 +0530

    Update budget_alert.tf

--- budget_alert.tf
+++ budget_alert.tf
@@ -2,20 +2,22 @@
 variable "resource_group_name" {
   description = "The name of the resource group containing the VMSS."
   type        = string
+  default     = "rg-vmss-budget-demo"
 }
  
 # Variable for the VMSS name
 variable "vmss_name" {
   description = "The name of the VMSS to monitor."
   type        = string
+  default     = "vmss-budget-demo"
 }
  
 # Budget alert configuration
 resource "azurerm_consumption_budget_subscription" "vmss_budget" {
   name            = "vmss-cost-budget"
   amount          = 50  # Set your desired budget amount in USD
-  time_grain      = "Monthly"
-  time_period_start = "2024-01-01T00:00:00Z"
+  time_grain      = "Daily"
+  time_period_start = "2025-08-01T00:00:00Z"
  
   # Filter to specifically monitor costs for the VMSS
   filter {
